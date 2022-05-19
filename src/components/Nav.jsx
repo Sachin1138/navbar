242f1006b8a178b95108917c0db57e473288ced7
Sachin1138 <99975080+Sachin1138@users.noreply.github.com>
2022-05-19 22:24:11 +0530

    xyz

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Nav = () =>{
+    return <div className='main'>
+        <div className='parent'>
+  <div id='first'>
+<a href='#' id='mor'>LOGOBAKERY</a>
+<a href='#'>Service</a>
+<a href='#'>Project</a>
+<a href='#'>About</a>
+  </div>
+
+  <div id='second'>
+<button href='#'>Contact</button>
+  </div>
+
+
+        </div>
+
+    </div>
+};
+export default Nav;
